3e70c515f0bd9426701943a8c105871aebc80eec
alexis-massa <alexis.massa351@gmail.com>
2024-07-27 11:42:13 +0200

    Completed

--- freeCodeCamp/fcctesting.js
+++ freeCodeCamp/fcctesting.js
@@ -32,9 +32,9 @@
  *
  */
 
-"use strict";
+"use strict"
 
-const fs = require("fs");
+const fs = require("fs")
 
 const allowedOrigins = [
   /^https?:\/\/([\w-]+\.)*freecodecamp.org/,
@@ -42,73 +42,73 @@ const allowedOrigins = [
   /^https:\/\/([\w-]+\.)*gitpod.io/,
   /^https:\/\/([\w-]+\.)*github.dev/,
   /^http:\/\/localhost:\d+/,
-];
+]
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    const origin = req.get("origin");
+    const origin = req.get("origin")
     if (allowedOrigins.some((regex) => regex.test(origin))) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-      console.log(origin);
+      res.setHeader("Access-Control-Allow-Origin", origin)
+      console.log(origin)
     }
 
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-  });
+    res.setHeader("Access-Control-Allow-Credentials", true)
+    next()
+  })
 
   app.route("/_api/app").get((req, res) => {
-    console.log("requested");
-    const appClone = { ...app };
-    res.json(appClone);
-  });
+    console.log("requested")
+    const appClone = { ...app }
+    res.json(appClone)
+  })
 
   app.route("/_api/server.js").get(function (req, res, next) {
-    console.log("requested");
+    console.log("requested")
     fs.readFile(process.cwd() + "/server.js", function (err, data) {
-      if (err) return next(err);
-      res.send(data.toString());
-    });
-  });
+      if (err) return next(err)
+      res.send(data.toString())
+    })
+  })
 
   app.route("/_api/routes.js").get(function (req, res, next) {
-    console.log("requested");
+    console.log("requested")
     fs.readFile(process.cwd() + "/routes.js", function (err, data) {
-      if (err) return next(err);
-      res.send(data.toString());
-    });
-  });
+      if (err) return next(err)
+      res.send(data.toString())
+    })
+  })
 
   app.route("/_api/auth.js").get(function (req, res, next) {
-    console.log("requested");
+    console.log("requested")
     fs.readFile(process.cwd() + "/auth.js", function (err, data) {
-      if (err) return next(err);
-      res.send(data.toString());
-    });
-  });
+      if (err) return next(err)
+      res.send(data.toString())
+    })
+  })
 
   app.route("/_api/package.json").get(function (req, res, next) {
-    console.log("requested");
+    console.log("requested")
     fs.readFile(process.cwd() + "/package.json", "utf-8", function (err, data) {
-      if (err) return next(err);
-      res.json(JSON.parse(data));
-    });
-  });
+      if (err) return next(err)
+      res.json(JSON.parse(data))
+    })
+  })
 
   app.get("/_api/app-info", function (req, res) {
     var hs = Object.keys(res._headers).filter(
       (h) => !h.match(/^access-control-\w+/)
-    );
-    var hObj = {};
+    )
+    var hObj = {}
     hs.forEach((h) => {
-      hObj[h] = res._headers[h];
-    });
-    delete res._headers["strict-transport-security"];
-    res.json({ headers: hObj });
-  });
+      hObj[h] = res._headers[h]
+    })
+    delete res._headers["strict-transport-security"]
+    res.json({ headers: hObj })
+  })
 
   app.get("/_api/app-stack", (req, res) => {
-    console.log("requested");
-    const stack = app._router.stack;
-    res.json(JSON.stringify(stack));
-  });
-};
+    console.log("requested")
+    const stack = app._router.stack
+    res.json(JSON.stringify(stack))
+  })
+}
--- server.js
+++ server.js
@@ -1,118 +1,98 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+'use strict'
+require('dotenv').config()
+const express = require('express')
+const myDB = require('./connection')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
 const session = require('express-session')
-const passport = require('passport');
-const { ObjectID } = require('mongodb')
-const LocalStrategy = require('passport-local');
+const passport = require('passport')
+const routes = require('./routes.js')
+const auth = require('./auth.js')
+const app = express()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const cookieParser = require('cookie-parser')
+const passportSocketIo = require('passport.socketio')
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
-const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
-fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
+fccTesting(app) // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Setup express session
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }))
 app.use(passport.initialize())
 app.use(passport.session())
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
+  const myDataBase = await client.db('database').collection('users')
 
-  // routes
-  app.route('/').get((req, res) => {
-    // render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true,
-      showRegistration: true
-    });
-  });
+  routes(app, myDataBase)
+  auth(app, myDataBase)
 
-  app.route('/login')
-    .post(
-      passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res) => {
-        res.redirect('/profile')
-      });
+  let currentUsers = 0;
+  io.on('connection', socket => {
+    ++currentUsers
 
-  app.route('/profile')
-    .get(
-      ensureAuthenticated, (req, res) => {
-        res.render('profile', { username: req.user.username })
-      })
-
-  app.route('/logout').get((req, res) => {
-    req.logOut(() => {
-      res.redirect('/');
-    });
-  });
-
-  app.route('register')
-    .post((req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) next(err)
-        else if (user) res.redirect('/')
-        else myDataBase.insertOne({ username: req.body.username, password: req.body.password })
-      }, (err, doc) => {
-        if (err) res.redirect('/')
-        else next(null, doc)
-      })
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
     })
 
-  // Error in case of 404
-  app.use((req, res, next) => {
-    res.status(404).type('text').send('Not found ')
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user', currentUsers);
+    })
+
+    socket.on('chat message', (data) => {
+      io.emit('chat message', { username, message })
+    })
   })
-
-  // Serialization and deserialization
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  // Define local strategy
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
-    });
-  }));
 
 }).catch(e => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
-  });
-});
+    res.render('index', { title: e, message: 'Unable to connect to database' })
+  })
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/')
+  accept(null, true);
 }
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+const PORT = process.env.PORT || 3000
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
